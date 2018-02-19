8a863baa4db0ff7bcb1978cb32330b6d8fd3ea67
161220049-Yicheng HUANG <453541724@qq.com>
2018-02-19 21:57:30 +0800

    users' information page anand image

--- app/models.py
+++ app/models.py
@@ -1,24 +1,32 @@
 from datetime import datetime
+from hashlib import md5
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), index=True, unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime)
+	
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
+	
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+	
+	def avatar(self, size):
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		return 'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'.format(digest=digest, size=size)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 
 @login.user_loader
@@ -29,7 +37,7 @@ def load_user(id):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
