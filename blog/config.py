7cd7486e86a29eb689d5ff88f7e937274873db25
161220049-Yicheng HUANG <453541724@qq.com>
2018-02-20 11:17:54 +0800

    support multipage

--- blog/config.py
+++ blog/config.py
@@ -13,4 +13,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['your-email@example.com']
-	POSTS_PER_PAGE = 25
+	POSTS_PER_PAGE = 10
