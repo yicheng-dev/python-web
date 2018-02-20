2c2fdbe016edf4a86d5baf44fb169f58fcf629cb
161220049-Yicheng HUANG <453541724@qq.com>
2018-02-20 14:20:51 +0800

    modify the UI and it's not so ugly.

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +15,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
 	if app.config['MAIL_SERVER']:
