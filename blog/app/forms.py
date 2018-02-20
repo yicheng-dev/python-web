70af16953ed4370a520be757f8576538bd542ecd
161220049-Yicheng HUANG <453541724@qq.com>
2018-02-20 10:57:20 +0800

    support multipage

--- blog/app/forms.py
+++ blog/app/forms.py
@@ -43,3 +43,7 @@ class EditProfileForm(FlaskForm):
 			user = User.query.filter_by(username = self.username.data).first()
 			if user is not None:
 				raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+	post = TextAreaField('Say something', validators=[DataRequired()])
+	submit = SubmitField('Submit')
