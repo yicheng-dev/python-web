34327d93e58a573744b24a603ce23f25fd57f390
161220049-Yicheng HUANG <453541724@qq.com>
2018-02-21 15:13:55 +0800

    modified:   TCP/echo/client.py 	modified:   TCP/echo/server.py 	modified:   blog/logs/blog.log

--- TCP/echo/client.py
+++ TCP/echo/client.py
@@ -6,12 +6,9 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 建立连接:
 s.connect(('127.0.0.1', 8080))
-# 接收欢迎消息:
 print s.recv(1024)
 for data in ['Michael', 'Tracy', 'Sarah']:
-    # 发送数据:
     s.send(data)
     print s.recv(1024)
 s.send('exit')
--- TCP/echo/server.py
+++ TCP/echo/server.py
@@ -18,13 +18,10 @@ def tcplink(sock, addr):
     print 'Connection from %s:%s closed.' % addr
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# 监听端口:
 s.bind(('127.0.0.1', 8080))
 s.listen(5)
 print 'Waiting for connection...'
 while True:
-    # 接受一个新连接:
     sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
     t = threading.Thread(target=tcplink, args=(sock, addr))
     t.start()
